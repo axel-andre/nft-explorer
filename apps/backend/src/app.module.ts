cfa4d681341f48485281e6a62219564705d7c9a7
Axel André <98.axel.andre@gmail.com>
2022-03-30 19:56:35 +0000

    feat(backend): add a new nfts feature

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NftsModule } from './nfts/nfts.module';
 
+console.log(process.env)
 @Module({
-  imports: [],
+  imports: [NftsModule, TypeOrmModule.forRoot({
+    url: process.env.DATABASE_URL,
+    type: 'postgres',
+    port: 5432
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
